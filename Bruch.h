f6c855920c7e647939ab8c4b36fcecb06ca07cf7
Xuantong Pan <xuantongpan@Xuantongs-MacBook-Pro.local>
2020-08-26 10:59:18 +0200

    first commit

---
+++ Bruch.h
@@ -0,0 +1,41 @@
+#include <iostream>       // f¸r ostream
+using namespace std ;
+
+
+/**
+  * Klasse f¸r Br¸che als Quotienten ganzer Zahlen
+  *
+  * Datenformat:
+  *		- Der Nenner ist echt positiv.
+  *		- Bei Z‰hler == 0 ist Nenner == 1.
+  *		- Z‰hler und Nenner sind ausgek¸rzt.
+  */
+
+class Bruch
+{
+/***  private Elemente ***/
+
+    // Z‰hler und Nenner des Objekts
+    long long _zaehler, _nenner ;
+
+    // stelle Datenformat des Objekts her
+    void _formatiere () ;
+
+public:
+/***  Konstruktoren  ***/
+
+
+/***  get-Methoden  ***/
+
+
+/***  Interaktionsmethoden, z.B. Vergleiche  ***/
+
+
+/***  Rechenmethoden  ***/
+
+
+} ;  // class Bruch
+
+
+/***  globale Funktionen  ***/
+
---
+++ main.cpp
@@ -0,0 +1,6 @@
+#include "Bruch.h"
+
+int main() {
+    cout << "Hello, World!" << endl;
+    return 0;
+}
